041d21ae0b31773970e789c9c5c470ff9866c52f
ReneLaverdure <renelaverdure@gmail.com>
2024-09-03 15:06:08 +1000

    added user auth methods

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
 
 const userSchema = mongoose.Schema({
     username: {
@@ -6,6 +9,15 @@ const userSchema = mongoose.Schema({
         required: [true, 'please provide a username'],
         min: [4, 'username must have a minimum of 4 characters'],
         max: [16, 'username cannot be longer than 16 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'please provide email'],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'please provide a valid email'
+        ],
+        unique: true
     },
     password: {
         type: String,
@@ -15,6 +27,22 @@ const userSchema = mongoose.Schema({
     }
 })
 
-const userModel = mongoose.model('user', userSchema)
 
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+userSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, username: this.username }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    const isMatch = await bcrypt.compare(password, this.password)
+    return isMatch
+}
+
+const userModel = mongoose.model('User', userSchema)
 module.exports = userModel
