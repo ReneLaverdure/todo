eb26b3281ebcf083f0e12540aa3b03eb0bfa89b0
ReneLaverdure <renelaverdure@gmail.com>
2024-09-03 15:06:33 +1000

    added register route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,17 @@
 const userModel = require('../models/userModel')
 
 exports.register = async (req, res) => {
-    console.log('register route')
-    res.status(200).send('register route')
+    try {
+
+        const user = await userModel.create({ ...req.body })
+        const token = user.createJWT()
+
+        res.status(200).json({ user: user.username, token })
+    } catch (err) {
+        console.log(err)
+        res.status(404).json({ errorMessage: 'register route error' })
+    }
+
 }
 
 exports.login = async (req, res) => {
